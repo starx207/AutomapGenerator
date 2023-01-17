260069a8101d7ac515798ed7610b967fe6a09097
Andrew Starr <astarr@appraisalresearch.cc>
2023-01-17 09:40:23 -0500

    Add PolySharp and remove IsExternalInit

--- src/AutomapGenerator.SourceGenerator/IsExternalInit.cs
+++
@@ -1,4 +0,0 @@
-﻿// Justification: https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-namespace System.Runtime.CompilerServices;
-internal class IsExternalInit {
-}
