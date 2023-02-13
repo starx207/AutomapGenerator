a9599bd9bb628d2a4509424758d0af2f0616c929
Andrew Starr <astarr@vgsi.com>
2023-02-13 11:32:51 -0500

    Modify how I'm including references for verification tests

--- test/AutomapGenerator.Generator.VerificationTests/Verifier.cs
+++ test/AutomapGenerator.Generator.VerificationTests/Verifier.cs
@@ -6,19 +6,26 @@ using XUnitVerifier = VerifyXunit.Verifier;
 
 namespace AutomapGenerator.Generator.VerificationTests;
 internal static class Verifier {
+    private static readonly string _coreLibPath = Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "{0}.dll");
+    private static readonly MetadataReference[] _references = new[] {
+            MetadataReference.CreateFromFile(string.Format(_coreLibPath, "mscorlib")),
+            MetadataReference.CreateFromFile(string.Format(_coreLibPath, "System")),
+            MetadataReference.CreateFromFile(string.Format(_coreLibPath, "System.Core")),
+            MetadataReference.CreateFromFile(string.Format(_coreLibPath, "System.Private.CoreLib")),
+            MetadataReference.CreateFromFile(string.Format(_coreLibPath, "System.Runtime")),
+            MetadataReference.CreateFromFile(string.Format(_coreLibPath, "System.Linq.Expressions")),
+            MetadataReference.CreateFromFile(typeof(MapProfile).Assembly.Location)
+        };
+
     public static Task Verify(string source, string directory, [CallerMemberName] string? testName = null) => Verify(new[] { source }, directory, testName);
 
     public static Task Verify(IEnumerable<string> sources, string directory, [CallerMemberName] string? testName = null) {
         var syntaxTrees = sources.Select(s => CSharpSyntaxTree.ParseText(s)).ToArray();
-        var references = new[] {
-            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-            MetadataReference.CreateFromFile(typeof(MapProfile).Assembly.Location)
-        };
 
         var compilation = CSharpCompilation.Create(
-            assemblyName: nameof(MapperGenerator_CreateMapVerifications),
+            assemblyName: "Mapper_Verifications",
             syntaxTrees: syntaxTrees,
-            references: references);
+            references: _references);
 
         var generator = new MapperGenerator();
         GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
